bcc1c43ec9b4bbe8d00c93403e359e70561e42e3
Thiago-Mota-Santos <lucimara1422@gmail.com>
2024-03-13 16:49:23 -0300

    fix(bug) dup pending tasks

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -1,12 +1,13 @@
 import { CheckCircle2, MinusCircle } from "lucide-react";
 import { ButtonContainer, Container, IconButton, TodoItem, TodoText, Tooltip, Text } from "./styles";
-import { ReactNode, useEffect, useRef, useState } from "react";
+import { Dispatch, ReactNode, SetStateAction, useEffect, useRef, useState } from "react";
 
 type ListProps = {
    list: string[];
    remove: (index: number, selectedButton: "pending" | "done") => void;
    edit: (index: number, newValue: string) => void;
-   complete: (index: number) => void;
+   complete: (index: number, selectedButton: "pending" | "done") => void;
+   setSelectButton: Dispatch<SetStateAction<string | null>>
    selectedButton: "pending" | "done";
 };
 
@@ -25,24 +26,6 @@ const TodoItemWithTooltip = ({ children, tooltipContent, onEdit, onDelete, index
    const [editable, setEditable] = useState(false);
    const textRef = useRef<HTMLDivElement>(null);
 
-   useEffect(() => {
-      const handleClickOutside = (event: MouseEvent) => {
-         if (textRef.current && !textRef.current.contains(event.target as Node)) {
-            setEditable(false);
-            const content = textRef.current.textContent || "";
-            if (content.trim() === "") {
-               onDelete(index); 
-            } else {
-               onEdit(index, content);
-            }
-         }
-      };
-
-      document.addEventListener("mousedown", handleClickOutside);
-      return () => {
-         document.removeEventListener("mousedown", handleClickOutside);
-      };
-   }, [index, onEdit, onDelete]);
 
    const handleMouseEnter = () => {
       if (!editable) {
@@ -119,10 +102,9 @@ const TodoItemWithTooltip = ({ children, tooltipContent, onEdit, onDelete, index
    );
 };
 
-export default function List({ list, remove, edit, complete, selectedButton }: ListProps) {
-  
+export default function List({ list, remove, edit, complete, selectedButton, setSelectButton }: ListProps) {
    const handleComplete = (index: number) => {
-      complete(index);
+      complete(index, selectedButton);
    };
 
    if (!list) return null;
@@ -135,9 +117,13 @@ export default function List({ list, remove, edit, complete, selectedButton }: L
       edit(index, newValue);
    };
 
+   const handleClearFilter = () => {
+     setSelectButton('')      
+   }
+
    return (
       <Container>
-        {!list.length ? <Text>There are no items marked as done. <span>Clear the filter here</span> to see all items </Text> : null}
+        {!list.length && selectedButton === 'done' ? <Text>There are no items marked as done. <button onClick={handleClearFilter}><span>Clear the filter here</span></button> to see all items </Text> : null}
          {list.map((item, index) => (
             <TodoItemWithTooltip key={index} tooltipContent="Edit item" onEdit={handleEdit} onDelete={handleDelete} index={index} onComplete={handleComplete} selectedButton={selectedButton}>
                {item}
